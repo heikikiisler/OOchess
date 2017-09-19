0ae97b4142ba253cf993397fdb0e57f408c09ea8
heiki.kiisler <heiki.kiisler@gmail.com>
2017-09-19 14:27:06 +0300

    Board with piece objects

--- src/piece/Empty.java
+++ src/piece/Empty.java
@@ -2,14 +2,14 @@ package piece;
 
 import side.Side;
 
-public class King extends Piece {
+public class Empty extends Piece {
 
-    public King(Side side, int row, int col) {
+    public Empty(Side side, int row, int col) {
         super(side, row, col);
     }
 
     @Override
     char getLetter() {
-        return 'k';
+        return '.';
     }
 }
