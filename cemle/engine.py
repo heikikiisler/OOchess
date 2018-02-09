14cfb752ed051fa5c33207661c4a96a09a012c8f
heikikiisler <heiki.kiisler@gmail.com>
2018-02-09 20:18:46 +0200

    Added basic evaluation methods for engine

--- cemle/engine.py
+++ cemle/engine.py
@@ -2,6 +2,8 @@ import random
 
 
 class Engine:
+    PIECE_VALUES = {"P": 1, "p": -1, "B": 3, "b": -3, "N": 3, "n": -3, "R": 5, "r": -5, "Q": 9, "q": -9}
+
     def __init__(self, board):
         self.board = board
 
@@ -12,4 +14,16 @@ class Engine:
         self.board.push(self.get_best_move())
 
     def get_board_evaluation(self):
-        return 0
+        return self.get_material_value()
+
+    def get_material_value(self):
+        material_sum = 0
+        fen_string = self.get_fen_piece_string()
+        for i in range(0, len(fen_string)):
+            char = fen_string[i]
+            if char in self.PIECE_VALUES.keys():
+                material_sum += self.PIECE_VALUES[char]
+        return material_sum
+
+    def get_fen_piece_string(self):
+        return self.board.fen().split(" ")[0]
